260a5925cbe6e957e1b92790d3cb1acd4e55ee7c
Craig Barnes <Cr@igBarn.es>
2015-04-28 03:39:27 +0100

    Add GPLv3 license header to tally.c

--- tally.c
+++ tally.c
@@ -1,3 +1,17 @@
+/*
+ Tally: a simple, command-line SLOC counter.
+ Copyright (C) 2014-2015 Craig Barnes.
+
+ This program is free software; you can redistribute it and/or modify it
+ under the terms of the GNU General Public License version 3, as published
+ by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ Public License version 3 for more details.
+*/
+
 #define _XOPEN_SOURCE 500 // For nftw(3)
 #define _GNU_SOURCE // For FTW_ACTIONRETVAL
 #include <ftw.h>
