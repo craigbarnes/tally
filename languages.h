95da6dc9cd95a9059ea1cf9c42b8778d579d2309
Craig Barnes <Cr@igBarn.es>
2017-05-26 16:52:24 +0100

    Add workaround to support building with gperf versions before 3.1
    The length type of the hash lookup functions generated by gperf changed
    from unsigned int to size_t in gperf 3.1. These changes allow passing a
    custom type as a preprocessor definition to fix any potential mismatch
    between headers and generated code when building with older versions.

--- languages.h
+++ languages.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "parse.h"
 
+#ifndef LENGTH_TYPE
+#define LENGTH_TYPE size_t
+#endif
+
 typedef enum {
     UNKNOWN = 0,
     IGNORED,
@@ -68,8 +72,8 @@ typedef struct {
     Language language;
 } LanguageHashSlot;
 
-const LanguageHashSlot *lookup_language_by_extension(const char*, size_t);
-const LanguageHashSlot *lookup_language_by_filename(const char*, size_t);
+const LanguageHashSlot *lookup_language_by_extension(const char*, LENGTH_TYPE);
+const LanguageHashSlot *lookup_language_by_filename(const char*, LENGTH_TYPE);
 const char *lookup_language_name(Language lang);
 Parser lookup_language_parser(Language);
 Language detect_language(const char *path, int base, int level, size_t size);
